a6a7a1316b0b5965976b77061f0bf1fe72c61fa0
fassa108 <sambndeyefassa@gmail.com>
2026-04-07 00:19:58 +0000

    Affichage des articles a l'accueil et du detail de chaque article(integralement) dans detail

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ from blog import views
 
 urlpatterns = [
     path('accueil/', views.accueil, name = 'accueil'),
+    path('detail/<int:pk>/', views.detail, name = 'detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from blog.models import Article
 
 # Create your views here.
 def accueil(request) :
-    return render(request,'accueil.html')
+    article = Article.objects.all()
+    context = {
+        'article' : article
+    }
+    return render(request,'accueil.html',context)
+
+def detail(request, pk) :
+    element = get_object_or_404(Article, pk = pk)
+    article = {
+        'article' : element
+    }
+    return render(request, 'detail.html',article)
