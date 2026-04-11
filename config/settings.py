daef8c91c8fb8ae43b903df42e29b71f64985a10
fassa108 <sambndeyefassa@gmail.com>
2026-04-11 09:21:40 +0000

    implementation de la fonctionnalite d'envoi de mail suite a une commentaire

--- config/settings.py
+++ config/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'sambndeyefassa@gmail.com'
-EMAIL_HOST_PASSWORD = 'xhju ytuk nepm opbl'
+EMAIL_HOST_PASSWORD =  os.getenv('PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
